06c24b6de48d1da373060384e6859f949a0bbacc
r3verb3r8 <johnrback@gmail.com>
2021-02-15 00:23:03 +0100

    Added searchform

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
 function App() {
+  const [document, setDocument] = useState({});
+  const [input, setInput] = useState("");
   useEffect(() => {
     axios
       .get("http://ansible/api/document/202911")
       .then((data) => {
-        console.log(data);
+        setDocument(data.data[0]);
+        console.log(data.data[0]);
       })
       .catch((err) => console.log(err));
   }, []);
+  // Event
+  const documentInput = (e) => {
+    setInput(e.target.value);
+  };
+  const searchDocument = () => {
+    axios.get(`http://ansible/api/document/${input}`).then((data) => {
+      console.log(data.data[0]);
+      setDocument(data.data[0]);
+    });
+  };
+
   return (
     <div>
-      <h1>FrontEnd</h1>
+      <div className="search">
+        <input onChange={documentInput} type="text" />
+        <button onClick={searchDocument}>Search</button>
+      </div>
+      <div className="document-info">
+        <h2>{document.doc_desc}</h2>
+        <h3>Ärendenummer: {document.doc_docno}</h3>
+        <h3>Riktning: {document.doc_category}</h3>
+        <h3>Process: {document.doc_process}</h3>
+        <p>Beskrivning: {document.doc_ca_desc}</p>
+      </div>
     </div>
   );
 }
